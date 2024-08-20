660e2a2cd6550373bc5f42055e9b420473360e0d
chris <pupu86019@gmail.com>
2024-08-20 23:31:01 +0800

    feat: add reducer logic with initial state and action handlers for adding, removing, and checking letters

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,5 +1,23 @@
 export const initialState = {
   currentWord: '',
+  previousGuesses: [],
+  answer: 'REACT',
+  message: '',
+};
+
+const validateWord = (guess, answer) => {
+  const result = [];
+
+  for (let i = 0; i < 5; i++) {
+    if (guess[i] === answer[i]) {
+      result.push('correct');
+    } else if (answer.includes(guess[i])) {
+      result.push('misplaced');
+    } else {
+      result.push('incorrect');
+    }
+  }
+  return result;
 };
 
 export const reducer = (state, action) => {
@@ -18,5 +36,37 @@ export const reducer = (state, action) => {
         ...state,
         currentWord: state.currentWord.slice(0, -1),
       };
+
+    case 'CHECK_ANSWER':
+      if (state.currentWord.length === 5) {
+        const result = validateWord(state.currentWord, state.answer);
+        const isCorrect = state.answer === state.currentWord;
+        console.log('Validation result:', result);
+
+        let message = '';
+
+        if (isCorrect) {
+          message = 'You win!';
+        } else if (state.previousGuesses.length === 5) {
+          message = 'You lose!';
+        }
+
+        return {
+          ...state,
+          previousGuesses: [
+            ...state.previousGuesses,
+            { word: state.currentWord, result },
+          ],
+          currentWord: '',
+          message,
+        };
+      }
+      return state;
+
+    case 'RESET':
+      return initialState;
+
+    default:
+      return state;
   }
 };
