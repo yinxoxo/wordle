b245cddbb97e1d241c128424062df0a96c80c153
chris <pupu86019@gmail.com>
2024-08-21 09:42:54 +0800

    refactor: rename reducer.js to gameReducer.js for better clarity

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useReducer } from 'react';
-import { reducer, initialState } from './reducer/reducer';
+import { reducer, initialState } from './reducer/gameReducer';
 import Grid from './components/Grid';
 import Keypad from './components/Keypad';
 
